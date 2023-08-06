5fd16af13fdeca7868bea9b8f84c17b8afb6f551
Ezaz Ahamed <ezazahamed3@gmail.com>
2023-08-07 03:04:20 +0600

    Project-almost done

---
+++ weatherJS.js
@@ -0,0 +1,65 @@
+
+        const apiKey = 'c36c3d4f715e64907aba33fa659344e5';
+
+        function weatherApi() {
+            const cityInput = document.getElementById('cityInput').value;
+            const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=metric&appid=${apiKey}`;
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => displayWeather(data))
+                .catch(error => console.log('Error:', error));
+        }
+
+        function displayWeather(data) {
+            const weatherInfoDiv = document.getElementById('weatherInfo');
+            weatherInfoDiv.innerHTML = '';
+
+            if (data.cod !== '200') {
+                weatherInfoDiv.innerHTML = '<p>Error: City not found</p>';
+                return;
+            }
+
+            const forecastList = data.list;
+            const forecastsFor6AM = forecastList.filter(forecast => {
+                const time = new Date(forecast.dt_txt).getHours();
+                return time === 6;
+            });
+
+            forecastsFor6AM.forEach(forecast => {
+                const dateContainer = document.createElement('div');
+                dateContainer.classList.add('date-container');
+
+                const dateTime = new Date(forecast.dt_txt);
+                const day = Week(dateTime.getDay());
+                const date = dateTime.toLocaleDateString();
+                dateContainer.innerHTML += `
+                    <h2>${day}, ${date}</h2><br>
+                `;
+
+                const temperature = forecast.main.temp;
+                const windSpeed = forecast.wind.speed;
+                const humidity = forecast.main.humidity;
+                const pressure = forecast.main.pressure;
+                const description = forecast.weather[0].description;
+                const iconCode = forecast.weather[0].icon;
+                const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+                dateContainer.innerHTML += `
+                    <p>Temperature: ${temperature}°C</p>
+                    <p>Wind Speed: ${windSpeed} m/s</p>
+                    <p>Humidity: ${humidity}%</p>
+                    <p>Pressure: ${pressure} hPa</p>
+                    <p>Description: ${description}</p>
+                    <img class="weather-icon" src="${iconUrl}" alt="Weather Icon">
+                    <hr>
+                `;
+
+                weatherInfoDiv.appendChild(dateContainer);
+            });
+        }
+
+        function Week(dayIndex) {
+            const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            return days[dayIndex];
+        }
